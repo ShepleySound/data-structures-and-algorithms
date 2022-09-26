4c2ab7bd03ef5f49eff2a261a856e5c12657d3d6
Robert Shepley <shepleysound@gmail.com>
2022-09-25 17:04:33 -0700

    Finish implementing basic linked list.

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,50 @@
 'use strict';
 
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require('../index');
 
 describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+  const list = new LinkedList();
+  test('Null when initialized', () => {
+    expect(list.traverseToEnd()).toBeNull();
+    expect(list.includes(5)).toEqual(false);
+    expect(list.toString()).toEqual('NULL');
+  });
+  test('Insert an integer', () => {
+    list.insertAtFront(5);
+    expect(list.traverseToEnd().data).toEqual(5);
+    expect(list.toString()).toEqual('{ 5 } -> NULL');
+  });
+  test('includes() returns true if a value exists', () => {
+    expect(list.includes(5)).toEqual(true);
+  });
+  test('includes() returns false if a value does not exist', () => {
+    expect(list.includes(5000)).toEqual(false);
+  });
+  test('Head property returns the first inserted value.', () => {
+    expect(list.head.data).toEqual(5);
+  });
+  test('Insert 5 integers, each at the front of the linked list.', () => {
+    const newList = new LinkedList();
+    newList.insertAtFront(1);
+    newList.insertAtFront(2);
+    newList.insertAtFront(3);
+    newList.insertAtFront(4);
+    newList.insertAtFront(5);
+    expect(newList.traverseToEnd().data).toEqual(1);
+    expect(newList.toString())
+      .toEqual('{ 5 } -> { 4 } -> { 3 } -> { 2 } -> { 1 } -> NULL');
+  });
+  test('Insert 5 integers, each at the back of the linked list.', () => {
+    const newList = new LinkedList();
+    newList.insertAtBack(1);
+    newList.insertAtBack(2);
+    newList.insertAtBack(3);
+    newList.insertAtBack(4);
+    newList.insertAtBack(5);
+    expect(newList.traverseToEnd().data).toEqual(5);
+    expect(newList.toString())
+      .toEqual('{ 1 } -> { 2 } -> { 3 } -> { 4 } -> { 5 } -> NULL');
   });
 });
+
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,69 @@
 'use strict';
 
-class LinkedList {
+class Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next ?? null;
+  }
+}
 
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  traverseToEnd() {
+    if (this.head) {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      return current;
+    } else return null;
+  }
+
+  insertAtFront(data) {
+    if (!this.head) {
+      this.head = new Node(data);
+    } else {
+      this.head = new Node(data, this.head);
+    }
+  }
+
+  insertAtBack(data) {
+    if (!this.head) {
+      this.head = new Node(data);
+    } else {
+      let current = this.traverseToEnd();
+      current.next = new Node(data);
+    }
+  }
+
+  includes(data) {
+    if (this.head) {
+      let current = this.head;
+      while (current) {
+        if (current.data === data) {
+          return true;
+        }
+        current = current.next;
+      }
+    }
+    return false;
+  }
+
+  toString() {
+    if (this.head) {
+      let current = this.head;
+      let string = '';
+      while (current) {
+        string += `{ ${current.data} } -> `;
+        current = current.next;
+      }
+      string += 'NULL';
+      return string;
+    } else return 'NULL';
+  }
 }
 
 module.exports = LinkedList;
