c6073d3eeb20d0f715a48bdc1c49ac6315e96341
Robert Shepley <shepleysound@gmail.com>
2022-09-25 17:34:02 -0700

    Add files for doubly linked list implementation

---
+++ javascript/doubly-linked-list/index.js
@@ -0,0 +1,88 @@
+'use strict';
+
+class Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next ?? null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  traverseToEnd() {
+    if (this.head) {
+      let current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      return current;
+    } else return null;
+  }
+
+  insertAtFront(data) {
+    if (!this.head) {
+      this.head = new Node(data);
+    } else {
+      const oldHead = this.head;
+      this.head = new Node(data, oldHead);
+      oldHead.previous = this.head;
+    }
+  }
+
+  insertAtBack(data) {
+    if (!this.head) {
+      this.head = new Node(data);
+    } else {
+      let current = this.traverseToEnd();
+      current.next = new Node(data, null, current);
+    }
+  }
+
+  includes(data) {
+    if (this.head) {
+      let current = this.head;
+      while (current) {
+        if (current.data === data) {
+          return true;
+        }
+        current = current.next;
+      }
+    }
+    return false;
+  }
+
+  getNode(data) {
+    if (this.head) {
+      let current = this.head;
+      while (current) {
+        if (current.data === data) {
+          return current;
+        }
+        current = current.next;
+      }
+    }
+    return null;
+  }
+
+  getPreviousNode(node) {
+    return node.previous ?? null;
+  }
+
+  toString() {
+    if (this.head) {
+      let current = this.head;
+      let string = '';
+      while (current) {
+        string += `{ ${current.data} } <-> `;
+        current = current.next;
+      }
+      string += 'NULL';
+      return string;
+    } else return 'NULL';
+  }
+}
+
+module.exports = DoublyLinkedList;
