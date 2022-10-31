bc45342715070c153ce8e93a265863ae9712edac
Robert Shepley <shepleysound@gmail.com>
2022-10-30 17:06:39 -0700

    Initialize data structure wireframe.

---
+++ javascript/hashtable/index.js
@@ -0,0 +1,28 @@
+'use strict';
+
+class HashTable {
+  constructor(bucketCount) {
+    this.table = bucketCount;
+    this.size = 0;
+  }
+
+  set(key, value) {
+
+  }
+
+  get(key) {
+
+  }
+
+  hash(key) {
+
+  }
+
+  has(key) {
+
+  }
+
+  keys() {
+
+  }
+}
